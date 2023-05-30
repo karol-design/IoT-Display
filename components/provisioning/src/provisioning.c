d3a2337369d425be71c285dd46e1d729c2845f01
Karol Wojslaw <wojslaw.tech@gmail.com>
2023-05-30 22:07:43 +0200

    Refactoring of provisioning code

--- components/provisioning/src/provisioning.c
+++ components/provisioning/src/provisioning.c
@@ -14,7 +14,6 @@
 #include "nvs_flash.h"
 #include "qrcode.h"
 #include "stdio.h"
-#include "string.h"
 #include "wifi_provisioning/manager.h"
 #include "wifi_provisioning/scheme_ble.h"
 
@@ -33,11 +32,8 @@ static EventGroupHandle_t wifi_event_group;
 #define QRCODE_BASE_URL "https://espressif.github.io/esp-jumpstart/qrcode.html"
 
 /* Event handler for catching system events */
-static void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data) {
-#ifdef CONFIG_EXAMPLE_RESET_PROV_MGR_ON_FAILURE
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     static int retries;
-#endif
     if (event_base == WIFI_PROV_EVENT) {
         switch (event_id) {
             case WIFI_PROV_START:
@@ -58,21 +54,17 @@ static void event_handler(void *arg, esp_event_base_t event_base,
                          "Provisioning failed!\n\tReason : %s"
                          "\n\tPlease reset to factory and retry provisioning",
                          (*reason == WIFI_PROV_STA_AUTH_ERROR) ? "Wi-Fi station authentication failed" : "Wi-Fi access-point not found");
-#ifdef CONFIG_EXAMPLE_RESET_PROV_MGR_ON_FAILURE
                 retries++;
                 if (retries >= CONFIG_EXAMPLE_PROV_MGR_MAX_RETRY_CNT) {
                     ESP_LOGI(TAG, "Failed to connect with provisioned AP, reseting provisioned credentials");
                     wifi_prov_mgr_reset_sm_state_on_failure();
                     retries = 0;
                 }
-#endif
                 break;
             }
             case WIFI_PROV_CRED_SUCCESS:
                 ESP_LOGI(TAG, "Provisioning successful");
-#ifdef CONFIG_EXAMPLE_RESET_PROV_MGR_ON_FAILURE
                 retries = 0;
-#endif
                 break;
             case WIFI_PROV_END:
                 /* De-initialize manager once provisioning is finished */
@@ -94,12 +86,23 @@ static void event_handler(void *arg, esp_event_base_t event_base,
     }
 }
 
+/**
+ * @brief Initializes Wi-Fi in station mode.
+ *
+ * @note Prior to calling this function, the necessary Wi-Fi configuration and credentials should be
+ *       set using the appropriate API functions.
+ */
 static void wifi_init_sta(void) {
-    /* Start Wi-Fi in station mode */
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
+/**
+ * @brief Generates a service name for the device based on the Wi-Fi MAC address.
+ *
+ * @param service_name Pointer to a buffer where the generated service name will be stored.
+ * @param max          The maximum size of the service name buffer.
+ */
 static void get_device_service_name(char *service_name, size_t max) {
     uint8_t eth_mac[6];
     const char *ssid_prefix = "PROV_";
@@ -108,36 +111,49 @@ static void get_device_service_name(char *service_name, size_t max) {
              ssid_prefix, eth_mac[3], eth_mac[4], eth_mac[5]);
 }
 
+/**
+ * @brief Generates and prints a QR code for provisioning.
+ *
+ * @param name      The device name for the QR code payload.
+ * @param pop       The proof of possession (pop) for the QR code payload.
+ * @param transport The transport type for the QR code payload.
+ */
 static void wifi_prov_print_qr(const char *name, const char *pop, const char *transport) {
-    if (!name || !transport) {
+    if (!name || !transport || !pop) {
         ESP_LOGW(TAG, "Cannot generate QR code payload. Data missing.");
         return;
     }
     char payload[150] = {0};
-    if (pop) {
-        snprintf(payload, sizeof(payload),
-                 "{\"ver\":\"%s\",\"name\":\"%s\""
-                 ",\"pop\":\"%s\",\"transport\":\"%s\"}",
-                 PROV_QR_VERSION, name, pop, transport);
-    } else {
-        snprintf(payload, sizeof(payload),
-                 "{\"ver\":\"%s\",\"name\":\"%s\""
-                 ",\"transport\":\"%s\"}",
-                 PROV_QR_VERSION, name, transport);
-    }
+
+    snprintf(payload, sizeof(payload),
+            "{\"ver\":\"%s\",\"name\":\"%s\""
+            ",\"pop\":\"%s\",\"transport\":\"%s\"}",
+            PROV_QR_VERSION, name, pop, transport);
+    
     ESP_LOGI(TAG, "Scan this QR code from the provisioning application for Provisioning.");
+
     esp_qrcode_config_t cfg = ESP_QRCODE_CONFIG_DEFAULT();
-    esp_qrcode_generate(&cfg, payload);
+    esp_err_t qrcode_err = esp_qrcode_generate(&cfg, payload);
+    if (qrcode_err != ESP_OK) {
+        ESP_LOGW(TAG, "Failed to generate QR code. Error code: %d", qrcode_err);
+        return;
+    }
+
     ESP_LOGI(TAG, "If QR code is not visible, copy paste the below URL in a browser.\n%s?data=%s", QRCODE_BASE_URL, payload);
 }
 
-esp_err_t provisioning_init(void) {
-    /* Initialize TCP/IP */
-    ESP_ERROR_CHECK(esp_netif_init());
+/**
+ * @brief Initializes provisioning and Wi-Fi connection.
+ *
+ * @param reset_provisioning Flag to reset Wi-Fi provisioning configuration if true.
+ * @return ESP_OK if initialization is successful.
+ */
+esp_err_t provisioning_init(bool reset_provisioning) {
+    ESP_ERROR_CHECK(esp_netif_init());  // Initialise TCP/IP Stack
 
     wifi_event_group = xEventGroupCreate();
 
-    /* Register our event handler for Wi-Fi, IP and Provisioning related events */
+    /* Register event handler for Wi-Fi, IP and Provisioning related events */
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_PROV_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
@@ -150,24 +166,24 @@ esp_err_t provisioning_init(void) {
     /* Configuration for the provisioning manager */
     wifi_prov_mgr_config_t config = {
         .scheme = wifi_prov_scheme_ble,                                       // Provision over BLE
-        .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM  // Free BLE Memory after prov
+        .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM  // Free BT Dual Mode mem after prov
     };
 
     /* Initialize provisioning manager with the configuration parameters set above */
     ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
 
-    bool provisioned = false;
-
-    /* -- Logic for deciding whether a reprovisioning is necessary -- */
-    if (true) {
-        wifi_prov_mgr_reset_provisioning();  // Reset Wi-Fi prov config (restore WiFi stack persistent settings to defaults)
+    /* If selected, reset Wi-Fi prov config (restore WiFi stack persistent settings to defaults) */
+    if (reset_provisioning) {
+        wifi_prov_mgr_reset_provisioning();
+        ESP_LOGI(TAG, "Reseting WiFi provisioning configuration");
     }
 
+    bool provisioned = false;
     ESP_ERROR_CHECK(wifi_prov_mgr_is_provisioned(&provisioned));
 
     /* If device is not yet provisioned start provisioning service */
     if (!provisioned) {
-        ESP_LOGI(TAG, "Starting provisioning");
+        ESP_LOGI(TAG, "Starting WiFi provisioning");
 
         char service_name[12];  // What is the Device Service Name (BLE device name)
         get_device_service_name(service_name, sizeof(service_name));
@@ -177,30 +193,14 @@ esp_err_t provisioning_init(void) {
          */
         wifi_prov_security_t security = WIFI_PROV_SECURITY_1;
 
-        const char *pop = "abcd1234";  // Proof-of-possession for security
+        const char *pop = "0000";       // Proof-of-possession for security
+        const char *service_key = NULL; // Ignored for provisioning over BLE
 
-        const char *service_key = NULL;  // Ignored for provisioning over BLE
-
-        // Set a custom 128 bit UUID (LSB <-> MSB)
+        // Set a custom 128 bit GATT service UUID (LSB <-> MSB)
         uint8_t custom_service_uuid[] = {
-            0xb4,
-            0xdf,
-            0x5a,
-            0x1c,
-            0x3f,
-            0x6b,
-            0xf4,
-            0xbf,
-            0xea,
-            0x4a,
-            0x82,
-            0x03,
-            0x04,
-            0x90,
-            0x1a,
-            0x02,
+            0xb4, 0xdf, 0x5a, 0x1c, 0x3f, 0x6b, 0xf4, 0xbf,
+            0xea, 0x4a, 0x82, 0x03, 0x04, 0x90, 0x1a, 0x02,
         };
-
         wifi_prov_scheme_ble_set_service_uuid(custom_service_uuid);
 
         /* Start provisioning service */
@@ -209,7 +209,7 @@ esp_err_t provisioning_init(void) {
         /* Print QR code for provisioning */
         wifi_prov_print_qr(service_name, pop, PROV_TRANSPORT_BLE);
     } else {
-        ESP_LOGI(TAG, "Already provisioned, starting Wi-Fi STA");
+        ESP_LOGI(TAG, "WiFi already provisioned, starting Wi-Fi STA");
         wifi_prov_mgr_deinit();  // Release Prov manager resources, as it's not needed
         wifi_init_sta();         // Start Wi-Fi station
     }
